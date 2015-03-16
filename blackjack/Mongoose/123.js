96e44042751d244e35d640acfef5ee8bcb61bdb8
Andrey Pavlov <paffa4@gmail.com>
2015-03-16 08:10:52 +0400

    Add mongoose for connect to DB

---
+++ blackjack/Mongoose/123.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose'),
+    options = require('../blackjackOptions.json');
+
+// intialize
+mongoose.set('debug', true);
+mongoose.connect(options.db);
+
+var db = mongoose.connection;
+db.on('error', function (err) {
+    console.error(err);
+});
+
+module.exports = mongoose;
