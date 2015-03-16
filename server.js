ab13e358dd7c3767573c0b70c96bbf18ffab1804
Andrey Pavlov <paffa4@gmail.com>
2015-03-16 14:57:20 +0000

    Blackjack game, initial commit

--- server.js
+++ server.js
@@ -8,5 +8,5 @@ requirejs.config({
     nodeRequire: require
 });
 
-app.use(express.static(path.resolve(__dirname, 'client')));
+app.use(express.static(path.resolve(__dirname, 'game')));
 app.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0");
